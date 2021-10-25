c54f09b5bf5455a07adb68bf5adb5fbf2dc92fe9
Kidus Worku <kidusmik@gmail.com>
2021-10-25 14:46:15 +0000

    Modified: simple_shell.c

--- simple_shell.c
+++ simple_shell.c
@@ -7,9 +7,12 @@
 */
 int main(__attribute__((unused)) int argc, __attribute__((unused)) char **argv, char **env)
 {
-	pid_t this_pid, hsh_pid;
+	pid_t this_pid, hsh_pid, child_pid;
 	char *hsh_pid_str, *hsh_pid_env_name, *this_pid_str;
-	int pid_length, env_count;
+	char *buffer, *delim, *params, *prompt, *comm_params[2];
+	int pid_length, env_count, status;
+	struct stat st;
+	size_t b_size;
 
 	hsh_pid_env_name = "HSHPID";
 	this_pid = getpid();
@@ -26,7 +29,45 @@ int main(__attribute__((unused)) int argc, __attribute__((unused)) char **argv, 
 	else
 		hsh_pid = str_to_pid(hsh_pid_str);
 
-	run_parent_shell(hsh_pid, this_pid, env);
+	comm_params[1] = NULL;
+	b_size = 32;
+	buffer = malloc(sizeof(char) * b_size);
+	delim = "\n";
+
+	if (this_pid > hsh_pid)
+		prompt = "($) ";
+	else
+		prompt = "$ ";
+
+	printf("%s", prompt);
+
+	while (getline(&buffer, &b_size, stdin) != -1)
+	{
+		params = strtok(buffer, delim);
+		comm_params[0] = params;
+
+		if (check_exit(comm_params[0]))
+			exit(0);
+
+		if (check_command(comm_params[0], &st, prompt))
+			continue;
+
+		child_pid = fork();
+
+		if (child_pid == -1)
+			return (1);
+
+		if (child_pid == 0)
+		{
+			if (execve(comm_params[0], comm_params, env) == -1)
+				return (1);
+		}
+		else
+		{
+			wait(&status);
+			printf("%s", prompt);
+		}
+	}
 
 	return (0);
 }
