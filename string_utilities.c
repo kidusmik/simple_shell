8c56f26fc5956b8036229c367283d61256962452
Kidus Worku <kidusmik@gmail.com>
2021-10-26 10:47:30 +0000

    Added: string_utilities.c

---
+++ string_utilities.c
@@ -0,0 +1,44 @@
+#include "hsh_main.h"
+
+void _strcat(char *path, char *command, char *command_path)
+{
+	int i, j;
+
+	i = 0;
+	j = 0;
+
+	while (*(path + i) != '\0')
+	{
+		*(command_path + i) = *(path + i);
+		i++;
+	}
+	*(command_path + i) = '/';
+	i++;
+
+	while (*(command + j) != '\0')
+	{
+		*(command_path + i) = *(command + j);
+		i++;
+		j++;
+	}
+	*(command_path + i) = '\0';
+}
+
+char * _strdup(char *from)
+{
+	int i, len;
+	char *dup_str;
+
+	len = _strlen(from) + 1;
+	dup_str = malloc(sizeof(int) * len);
+	i = 0;
+
+	while (*(from + i) != '\0')
+	{
+		*(dup_str + i) = *(from + i);
+		i++;
+	}
+	*(dup_str + i) = '\0';
+
+	return (dup_str);
+}		
